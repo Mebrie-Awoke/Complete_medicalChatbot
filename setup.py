08cf28b2eed3c9e2402e54aa49d92398723256a7
Mebrie-Awoke <mebrieawoke941@gmail.com>
2026-02-26 23:07:04 +0300

    Add all project files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Mebrie, Awoke",
+    author_email="mebrieawoke941@gmail.com",
+    packages=find_packages(),
+
+)
