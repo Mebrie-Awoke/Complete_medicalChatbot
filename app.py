3ac5e5a43f6c99da85f66ca0be449ef62c0d9ce7
Mebrie Awoke <mebrieawoke941@gmail.com>
2026-08-05 19:38:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 from src.prompt import *
-import os  
+import os   
 
 app = Flask(__name__)
 
